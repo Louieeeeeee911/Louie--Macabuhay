03bccc4c1b46abc0c2d7a7a956323c7a83c3d690
Louieeeeeee911 <lmacabuhay2018@gmail.com>
2024-12-22 18:40:11 +0800

    Update LA#26.py

--- LA/26/LA#26.py
+++ LA/26/LA#26.py
@@ -41,15 +41,3 @@ class Raphael(NinjaTurtle):
     @property
     def alias(self):
         return f"{self.real_name}, {self.__alias}"
-
-import oop
-
-leonardo = oop.Leonardo("Leonardo", "Leo")
-michaelangelo = oop.Michaelangelo("Michaelangelo", "Gelo")
-donatello = oop.Donatello("Donatello", "Dona")
-raphael = oop.Raphael("Raphael", "Paeng")
-
-print(leonardo.alias)
-print(michaelangelo.alias)
-print(donatello.alias)
-print(raphael.alias)
