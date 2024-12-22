455fa37a223b2f3d182588af70253f91c6a0d716
Louieeeeeee911 <lmacabuhay2018@gmail.com>
2024-12-22 18:40:51 +0800

    Create file 2.py

---
+++ LA/26/file 2.py
@@ -0,0 +1,11 @@
+import oop
+
+leonardo = oop.Leonardo("Leonardo", "Leo")
+michaelangelo = oop.Michaelangelo("Michaelangelo", "Gelo")
+donatello = oop.Donatello("Donatello", "Dona")
+raphael = oop.Raphael("Raphael", "Paeng")
+
+print(leonardo.alias)
+print(michaelangelo.alias)
+print(donatello.alias)
+print(raphael.alias)
